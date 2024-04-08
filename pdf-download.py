5527b409862560aea5bdfacc38d8da7e77fe0d67
Nathaniel174 <nathaniel.wojtalik@icloud.com>
2024-04-09 00:29:46 +0200

    small changes

--- pdf-download.py
+++ pdf-download.py
@@ -3,18 +3,20 @@
 # TODO: nur erste Seite runterladen 
 
 import os
+from os import listdir
+from os.path import isfile , join 
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
-# for random 4 PDF-files
-# import random
+import random
 
 # Set the Url of the main Website 
 url = "https://swgdrug.org/monographs.htm"
 
 # Create an Array with urls of every PDF 
 fileUrls = []
+filePaths = []
 
 # Print every PDF-Url in CLI
 def print_all_html_filelinks():
@@ -61,10 +63,16 @@ def download_pdf_files(fileUrl):
             pdf_object.write(response.content)
             # positive feedback 
             print(pdf_file_name, ' successfully downloaded in directory.')
+            
+        filePaths.append(filepath)
     else:
         # negative feedback
         print('This file could not be downloaded: ', pdf_file_name)
         print('HTTP response status code: ',response.status_code)
+
+
+# cut PDF-file that only first page exists 
+
 
 # -------- main ---------
 # get every Link and download all PDFs into local folder: 
@@ -85,12 +93,15 @@ main()
 
 
 # --------- dev-tools ----------
-# Print all links and total number:
-# get_all_pdf_links()
+#
 # print_all_html_filelinks()
 # print(len(fileUrls))
-
+get_all_pdf_links()
+#
 # Download 4 example files:
-# for i in range(0,4):
-#     tmp = random.randint(0,len(fileUrls))
-#     download_pdf_files(fileUrls[tmp])    
+for i in range(0,4):
+    tmp = random.randint(0,len(fileUrls))
+    download_pdf_files(fileUrls[tmp])
+print(filePaths)
+    
+# get_all_pdf_paths()
